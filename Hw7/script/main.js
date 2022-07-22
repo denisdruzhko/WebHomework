094bcedfdd50726e70b6630148b8a73a1d85cd74
mvper7 <denisdruzhko@gmail.com>
2022-07-22 20:19:46 +0300

    HW7

---
+++ Hw7/script/main.js
@@ -0,0 +1,25 @@
+
+let burger = document.querySelector('.burger')
+let menu = document.querySelector('.menu')
+burger.addEventListener('click',function(e){
+    burger.classList.toggle('burger-active')
+    menu.classList.toggle('menu-active')
+    
+    // if(menu.classList.contains('menu-active')){
+    //     menu.classList.remove('menu-active')
+    // }else{
+    //     menu.classList.add('menu-active')
+    // }
+
+})
+
+
+Fancybox.bind("#gallery a", {
+    groupAll : true, 
+    
+  });
+
+const myCarousel = new Carousel(document.querySelector(".carousel"), {
+    'center' : true,
+
+  });
